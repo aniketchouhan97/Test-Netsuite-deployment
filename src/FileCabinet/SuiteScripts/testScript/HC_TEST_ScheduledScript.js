bb10675bd898647e16abf48c47718f18d8f92807
aniket12chouhan <aniket12chouhan@gmail.com>
2026-05-31 22:58:30 +0530

    chore: comment out execution logic in HC_TEST_ScheduledScript.js

--- src/FileCabinet/SuiteScripts/testScript/HC_TEST_ScheduledScript.js
+++ src/FileCabinet/SuiteScripts/testScript/HC_TEST_ScheduledScript.js
@@ -5,17 +5,17 @@
 define(['N/log'],
     (log) => {
         const execute = (context) => {
-            try {
-                log.debug({
-                    title: 'Test Deployment Script',
-                    details: 'Scheduled script executed successfully'
-                });
-            } catch (error) {
-                log.error({
-                    title: 'Error in Test Deployment Script',
-                    details: error.toString()
-                });
-            }
+            // try {
+            //     log.debug({
+            //         title: 'Test Deployment Script',
+            //         details: 'Scheduled script executed successfully'
+            //     });
+            // } catch (error) {
+            //     log.error({
+            //         title: 'Error in Test Deployment Script',
+            //         details: error.toString()
+            //     });
+            // }
         };
 
         return { execute };
